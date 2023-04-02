71ece84415d7872d4d3145b4e30e5401ec6b35ca
Alex Fera <alexandruf@stiona.com>
2023-04-02 22:48:33 +0300

    Added a health check continue

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    run(listener)?.await
 }
