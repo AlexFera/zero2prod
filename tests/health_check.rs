26e629698c147d2122a3fd4c802d3f428ca97dfb
Alexandru Fera <alexandrufera@gmail.com>
2023-04-04 12:39:38 +0300

    Fixed clippy warning

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -10,7 +10,7 @@ fn spawn_app() -> String {
     // Launch the server as a background task
     // tokio::spawn returns a handle to the spawned future,
     // but we have no use for it here, hence the non-binding let
-    let _ = tokio::spawn(server);
+    tokio::spawn(server);
 
     format!("http://127.0.0.1:{}", port)
 }
