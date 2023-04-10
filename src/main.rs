d4e7f8c20680db022a68ac4b7d1a7272672ed097
Alexandru Fera <alexandrufera@gmail.com>
2023-04-10 17:11:39 +0300

    Set timeout

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
 
     let connection_pool = PgPoolOptions::new()
-        // `connect_lazy_with` instead of `connect_lazy`
+        .acquire_timeout(std::time::Duration::from_secs(2))
         .connect_lazy_with(configuration.database.with_db());
 
     let address = format!(
